5e04bec6daeb5cc70e11dc12264051a58ddfe5d7
Teresa Rokos <teresarokos@Teresas-MacBook-Air.local>
2018-11-12 14:18:10 -0500

    "functions from class"

---
+++ Class11-12-18.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+
+# Q1: convert "CA-49-1" to "elections-poll-ca49-1.csv"
+
+download.file(url = "https://github.com/TheUpshot/2018-live-poll-results/tree/master/data",
+              destfile = "master.zip",
+              quiet = TRUE,
+              mode = "wb")
+
+
+
+build_filename <- function(x){
+  separate(x, into = c(state, district, wave), sep = "-")
+  paste0("elections-poll-", str_to_lower(state), district, "-", wave, ".csv")
+}
+
+# Q2
+get_path <- function(x){
+  separate(x, into = c(state, district, wave), sep = "-")
+  paste0("https://github.com/TheUpshot/2018-live-poll-results/blob/master/data/elections-poll-", 
+         str_to_lower(state), district, "-", wave, ".csv")
+}
+
+get_path <- function(district, wave){
+  paste0("https://github.com/TheUpshot/2018-live-poll-results/blob/master/data/elections-poll-", 
+         district, "-", wave, ".csv")
+}
+
+
+# Q3
+get_csv_link <- function(district, wave){
+  paste0("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-", 
+         district, "-", wave, ".csv")
+}
+
+table_poll <- function(district, wave){
+  poll_link <- get_csv_link(district, wave)
+  poll <- read_csv(poll_link, col_names = TRUE)
+  poll
+}
+
